b5a1a39fceff5d2c0476fb8d6fd9270fd89ef25c
이승연 <leelaf0002@gmail.com>
2025-07-08 23:26:24 +0900

    title_music 리소스 추가, 사운드클래스 생성

---
+++ sfml-zombie/SoundGo.h
@@ -0,0 +1,27 @@
+﻿class SoundGo :
+	public Singleton<SoundGo>
+{
+protected:
+	std::string soundId;
+
+	sf::SoundBuffer bufferHit;
+	sf::SoundBuffer bufferPickUp;
+
+	sf::Sound soundHit;
+	sf::Sound soundPickUp;
+
+	bool soundMute;
+	bool currentMuteCheck;
+
+public:
+	SoundGo();
+	~SoundGo();
+
+	void Init();
+	void Play(std::string id);
+	void MuteSet(bool mute);
+	bool MuteGet();
+};
+
+#define SOUND_MGR (SoundGo::Instance())
+
